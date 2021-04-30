0eafcda7a51091695e8a06f975683a6930a7d4b5
TalithadeKramer <76871148+TalithadeKramer@users.noreply.github.com>
2021-04-30 10:28:16 +0200

    Add files via upload

---
+++ Opdracht_033/script.js
@@ -0,0 +1,43 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+};
+ikRockArrowFunctions()
+
+
+
+
+const ikRockArrowFunctions2 = () => console.log("Joe, ik rock de arrow functions!");
+ikRockArrowFunctions2()
+
+
+
+
+// zet de functie om naar een arrow functie
+const fivePlusSeven = function () {
+    return 5 + 7
+};
+fivePlusSeven();
+
+
+
+
+const fivePlusSeven2 = () => 5 + 7;
+fivePlusSeven2();
+
+
+
+
+const myFunction = (a, b) => a + b;
+myFunction();
+
+
+
+const addFive = a => a + 5;
+addFive();
+
+
+
+
+const createObject = () => ({ greeting: "hoi" })
+createObject() 
